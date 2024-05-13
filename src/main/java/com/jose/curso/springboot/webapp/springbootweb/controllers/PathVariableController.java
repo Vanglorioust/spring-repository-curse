c363620822bca110dae1243f6efae16e856bb726
Vanglorioust <jose.baza@correo.usa.edu.co>
2024-05-12 21:08:03 -0500

    Spring Curse - 5.12

--- src/main/java/com/jose/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/jose/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -3,9 +3,12 @@ package com.jose.curso.springboot.webapp.springbootweb.controllers;
 import com.jose.curso.springboot.webapp.springbootweb.models.User;
 import com.jose.curso.springboot.webapp.springbootweb.models.dto.ParamDTO;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,6 +23,13 @@ public class PathVariableController {
     private String[] listOfValues;
     @Value("${config.code}")
     private Integer code;
+    @Value("#{'${config.listOfValues}'.toUpperCase().split(',')}") // Split the values by comma and convert to uppercase
+    private List<String> valuesList;
+    @Value("#{'${config.listOfValues}'}")
+    private String values;
+    @Value("#{${config.valuesMap}}") // Convert the values to a map
+    private Map<String,Object> valuesMap;
+
 
     @GetMapping("baz/{message}")
      public ParamDTO baz(@PathVariable String message){
@@ -47,7 +57,10 @@ public class PathVariableController {
         json.put("username",username);
         json.put("message",message);
         json.put("listOfValues",listOfValues);
+        json.put("values",values);
+        json.put("valuesList",valuesList);
         json.put("code",code);
+        json.put("valuesMap",valuesMap);
         return json;
     }
 
